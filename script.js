9d4ecba206b287f49f8f1f7185241e7e6430dac0
Sharz <rikigamer160@gmail.com>
2022-09-17 16:04:32 +0200

    last commit

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-// FIND YOUR IP AND LOCATION WHEN OPEN WEBSITE
+// GLOBAL VARIABLES
 
 let inputField = document.querySelector(".input-field");
 let enterBtn = document.querySelector(".enter-btn");
 let ipAddress = document.querySelector(".ip");
-let locatioM = document.querySelector(".location");
+let locationVar = document.querySelector(".location");
 let timeZone = document.querySelector(".time-zone");
 let isp = document.querySelector(".isp");
+var map = undefined;
+var marker = undefined;
+var popup = undefined;
+
+// FIND YOUR IP AND LOCATION WHEN OPEN WEBSITE
+
+fetch(
+  `https://geo.ipify.org/api/v2/country,city?apiKey=at_8Y9wyUdzklq4yvF4m2aG6B7tm072h`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    // INITIALIZE THE MAP
+
+    map = L.map("map").setView([data.location.lat, data.location.lng], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution: "© OpenStreetMap",
+    }).addTo(map);
+
+    marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+
+    popup = L.popup()
+      .setLatLng([data.location.lat, data.location.lng])
+      .setContent("Your Position")
+      .openOn(map);
+
+    // PRINT INFO
+
+    ipAddress.innerHTML = data.ip;
+    locationVar.innerHTML = data.location.city;
+    timeZone.innerHTML = data.location.timezone;
+    isp.innerHTML = data.isp;
+  });
+
+// FIND INFORMATION FOR A SEARCHED IP ADDRESS OR DOMAIN
 
 enterBtn.addEventListener("click", () => {
   if (inputField.value === "") {
@@ -13,7 +49,7 @@ enterBtn.addEventListener("click", () => {
   } else {
     // GET IP ADDRESS AND LOCATION
     fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_8Y9wyUdzklq4yvF4m2aG6B7tm072h&ipAddress=${inputField.value}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_8Y9wyUdzklq4yvF4m2aG6B7tm072h&ipAddress=${inputField.value}&domain=${inputField.value}`
     )
       .then((response) => {
         if (!response.ok) {
@@ -23,22 +59,27 @@ enterBtn.addEventListener("click", () => {
         return response.json();
       })
       .then((data) => {
-        console.log(data.location);
-        var map = L.map("map").setView(
-          [data.location.lat, data.location.lng],
-          13
-        );
+        console.log(data);
+
+        // INITIALIZE THE MAP
+
+        map.setView([data.location.lat, data.location.lng], 13);
 
         L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
           maxZoom: 19,
           attribution: "© OpenStreetMap",
         }).addTo(map);
 
-        var marker = L.marker([data.location.lat, data.location.lng]).addTo(
-          map
-        );
+        L.marker([data.location.lat, data.location.lng]).addTo(map);
+
+        // PRINT INFO
+
+        ipAddress.innerHTML = data.ip;
+        locationVar.innerHTML = data.location.city;
+        timeZone.innerHTML = data.location.timezone;
+        isp.innerHTML = data.isp;
       });
 
-    // INITIALIZE THE MAP
+    inputField.value = "";
   }
 });
